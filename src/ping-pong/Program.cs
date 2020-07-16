533723ac184ac092681dc1166363c42175be64fb
Mikhail Tripolitov <tripolitov@gmail.com>
2020-07-16 11:43:26 +0300

    Add readme and empty console project

---
+++ src/ping-pong/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ping_pong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
