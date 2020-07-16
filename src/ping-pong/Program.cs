d6371389e0b747d7f1296bbda0eba362af6ddd58
Mikhail Tripolitov <tripolitov@gmail.com>
2020-07-16 12:06:07 +0300

    Add ping-pong implementation using two instances of AutoResetEvent

--- src/ping-pong/Program.cs
+++ src/ping-pong/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace ping_pong
 {
@@ -6,7 +7,59 @@ namespace ping_pong
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Ссигнализации для сотановки приложения
+            // и завершения рабочих потоков
+            var stop = new ManualResetEvent(initialState: false);
+
+            // Создание событий для синхронизации потоков.
+            //
+            // Особеность AutoResetEvent в том что при получении 
+            // сигнала и освобождении одного потока он автоматически
+            // сбрасывается в несигнальное состояние.
+            //
+            // ping создаётся в изначально сигнальном состоянии
+            // что бы обеспечить выполнение условия что ping
+            // выводится первым
+            var ping = new AutoResetEvent(initialState:true);
+            // pong создаётся в изначально несигнальном состоянии
+            // что бы обеспечить выполнение условия что ping
+            // выводится первым
+            var pong = new AutoResetEvent(initialState: false);
+
+            // Создание рабочего потока для вывода строки "ping"
+            var pinger = new Thread(() =>
+            {
+                while (!stop.WaitOne(0) && ping.WaitOne())
+                {
+                    Console.WriteLine("ping");
+                    pong.Set(); // уведомление для ponger что можно вывести pong
+                }
+            });
+
+            // Создание рабочего потока для вывода строки "pong"
+            var ponger = new Thread(() =>
+            {
+                while (!stop.WaitOne(0) && pong.WaitOne())
+                {
+                    Console.WriteLine("pong");
+                    ping.Set(); // уведомление для pinger что можно вывести ping
+                }
+            });
+
+            // Запуск рабочих потоков
+            pinger.Start();
+            ponger.Start();
+
+            // Ожидание пользовательского ввода для остановки приложения
+            Console.ReadLine();
+
+            // Установка флага остановки приложения сигнализирует всем
+            // рабочим потокам об остановке
+            stop.Set();
+
+            // Ожидание завершения рабочих потоков 
+            pinger.Join();
+            ponger.Join();
         }
     }
 }
